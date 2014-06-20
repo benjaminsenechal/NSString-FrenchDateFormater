0e3c5ace30448e28acf85c34b3fa27a427d091e0
benjaminsenechal <benjamin.senechal1@epsi.fr>
2014-06-20 09:54:05 +0200

    Update README.md

--- NSString+FrenchDateFormater.m
+++ NSString+FrenchDateFormater.m
@@ -12,23 +12,18 @@
 
 - (NSString *)makeDateWithFormat:(NSString *)format{
     if ([format isEqual:@"yyyy-MM-ddTHH:mm:ssZ"]){
-        NSArray *myWords = [self componentsSeparatedByString:@"T"];
-        NSString *date = [myWords objectAtIndex:0];
-        NSArray *myDate = [date componentsSeparatedByString:@"-"];
-        NSString *a = [myDate objectAtIndex:0];
-        NSString *m = [myDate objectAtIndex:1];
-        NSString *j = [myDate objectAtIndex:2];
-        return [NSString stringWithFormat: @"%@/%@/%@", j, m,a];
+        NSRange year = NSMakeRange(0, 4);
+        NSRange month = NSMakeRange(5, 2);
+        NSRange day = NSMakeRange(8, 2);
+        return [NSString stringWithFormat: @"%@/%@/%@", [self substringWithRange:day], [self substringWithRange:month], [self substringWithRange:year]];
     }else if ([format isEqual:@"YYYYMMDD"])
     {
         NSRange year = NSMakeRange(0, 4);
         NSRange month = NSMakeRange(4, 2);
         NSRange day = NSMakeRange(6, 2);
         return [NSString stringWithFormat: @"%@/%@/%@", [self substringWithRange:day], [self substringWithRange:month], [self substringWithRange:year]];
-    }else{
-        return @"Format not yet supported";
     }
-    return @"";
+    return [[NSString alloc] init];
 }
 
 @end
--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # NSString+FrenchDateFormater 
 
+Convert your date easily.
+
 ## Usage
 
+```
+    NSString *dateIso = @"2013-04-15T19:24:49Z";
+    [dateIso makeDateWithFormat:@"yyyy-MM-ddTHH:mm:ssZ"]; // return 15/04/2013
+    
+    NSString *dateIso = @"20140423";
+    [dateIso makeDateWithFormat:@"YYYYMMDD"]; // return 23/04/2014
+```
