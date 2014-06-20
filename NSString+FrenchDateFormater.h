0fa7b4282691a8de7e52e9aede38fc5819c405b4
benjaminsenechal <benjamin.senechal1@epsi.fr>
2014-06-20 09:45:09 +0200

    add makeDateWithFormat function

--- NSString+FrenchDateFormater.h
+++ NSString+FrenchDateFormater.h
@@ -10,4 +10,6 @@
 
 @interface NSString (FrenchDateFormater)
 
+- (NSString *)makeDateWithFormat:(NSString *)format;
+
 @end
--- NSString+FrenchDateFormater.m
+++ NSString+FrenchDateFormater.m
@@ -10,4 +10,25 @@
 
 @implementation NSString (FrenchDateFormater)
 
+- (NSString *)makeDateWithFormat:(NSString *)format{
+    if ([format isEqual:@"yyyy-MM-ddTHH:mm:ssZ"]){
+        NSArray *myWords = [self componentsSeparatedByString:@"T"];
+        NSString *date = [myWords objectAtIndex:0];
+        NSArray *myDate = [date componentsSeparatedByString:@"-"];
+        NSString *a = [myDate objectAtIndex:0];
+        NSString *m = [myDate objectAtIndex:1];
+        NSString *j = [myDate objectAtIndex:2];
+        return [NSString stringWithFormat: @"%@/%@/%@", j, m,a];
+    }else if ([format isEqual:@"YYYYMMDD"])
+    {
+        NSRange year = NSMakeRange(0, 4);
+        NSRange month = NSMakeRange(4, 2);
+        NSRange day = NSMakeRange(6, 2);
+        return [NSString stringWithFormat: @"%@/%@/%@", [self substringWithRange:day], [self substringWithRange:month], [self substringWithRange:year]];
+    }else{
+        return @"Format not yet supported";
+    }
+    return @"";
+}
+
 @end
