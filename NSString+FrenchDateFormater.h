b953dc2daa34588f003ca371925ec4c4b984cf57
benjaminsenechal <benjamin.senechal1@epsi.fr>
2014-06-20 09:29:10 +0200

    first commit

---
+++ NSString+FrenchDateFormater.h
@@ -0,0 +1,13 @@
+//
+//  NSString+FrenchDateFormater.h
+//  TimerWithBlock
+//
+//  Created by Benjamin SENECHAL on 20/06/2014.
+//  Copyright (c) 2014 Benjamin SENECHAL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (FrenchDateFormater)
+
+@end
---
+++ NSString+FrenchDateFormater.m
@@ -0,0 +1,13 @@
+//
+//  NSString+FrenchDateFormater.m
+//  TimerWithBlock
+//
+//  Created by Benjamin SENECHAL on 20/06/2014.
+//  Copyright (c) 2014 Benjamin SENECHAL. All rights reserved.
+//
+
+#import "NSString+FrenchDateFormater.h"
+
+@implementation NSString (FrenchDateFormater)
+
+@end
