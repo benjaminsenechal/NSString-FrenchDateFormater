d01257dbc795c0be2a585301a7edbd620187f352
benjaminsenechal <benjamin.senechal1@epsi.fr>
2014-06-23 17:26:58 +0200

    Add YYYYMM format

--- NSString+FrenchDateFormater.m
+++ NSString+FrenchDateFormater.m
@@ -11,17 +11,22 @@
 @implementation NSString (FrenchDateFormater)
 
 - (NSString *)makeDateWithFormat:(NSString *)format{
-    if ([format isEqual:@"yyyy-MM-ddTHH:mm:ssZ"]){
+    if ([format isEqual:@"yyyy-MM-ddTHH:mm:ssZ"]) {
         NSRange year = NSMakeRange(0, 4);
         NSRange month = NSMakeRange(5, 2);
         NSRange day = NSMakeRange(8, 2);
         return [NSString stringWithFormat: @"%@/%@/%@", [self substringWithRange:day], [self substringWithRange:month], [self substringWithRange:year]];
-    }else if ([format isEqual:@"YYYYMMDD"])
-    {
+    } else if ([format isEqual:@"YYYYMMDD"]) {
         NSRange year = NSMakeRange(0, 4);
         NSRange month = NSMakeRange(4, 2);
         NSRange day = NSMakeRange(6, 2);
         return [NSString stringWithFormat: @"%@/%@/%@", [self substringWithRange:day], [self substringWithRange:month], [self substringWithRange:year]];
+    } else if ([format isEqual:@"YYYYMM"]) {
+        NSRange year = NSMakeRange(0, 4);
+        NSRange month = NSMakeRange(4, 2);
+        return [NSString stringWithFormat: @"%@/%@/%@", [self substringWithRange:month], [self substringWithRange:year]];
+    } else {
+        @"Not yet supported";
     }
     return [[NSString alloc] init];
 }
